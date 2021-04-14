5ecf3b5423ec647d33155732d9257311af5e810a
ArthurNNN <nikitinarthur@gmail.com>
2021-04-14 23:10:56 +0200

    Part 2 finished

--- js/pull-requests.js
+++ js/pull-requests.js
@@ -1,52 +1,43 @@
-// const getPRs = function (URL) {
-//     return fetch(URL)
-//         .then(data => data.json())
-//         .then(function (response) {
-//             var ul = document.querySelector("#pull-requests-list");
-//             var userName = document.querySelector("#userName").value.trim();
-//             return response.map(function (pr) {
-//                 // console.log(rep.title);
-//                 // if (pr.user.login === userName) {
-//                 var li = document.createElement("li");
-//                 var a = document.createElement("a");
-//                 a.innerText = pr.title;
-//                 a.href = pr.url;
-//                 li.appendChild(a);
-//                 ul.appendChild(li);
-//                 return pr.title;
-//                 // }
+function postArray(array) {
+  var ul = document.querySelector("#pull-requests-list");
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+  array.forEach(item => {
+    var li = document.createElement("li");
+    var a = document.createElement("a");
+    a.innerText = item.login + " - " + item.title;
+    a.href = item.url;
+    li.appendChild(a);
+    ul.appendChild(li);
+  });
+}
 
-//             });
-//         });
-// };
-
-// input is a DOM element we already have using "querySelector"
-var input = document.querySelector("#userName");
-input.addEventListener("keyup", event => {
-  // (URL) => {
-  const value = event.target.value;
-  console.log(value);
-  return fetch(URL)
-    .then(data => data.json())
-    .then(function(response) {
-      var ul = document.querySelector("#pull-requests-list");
-      // var userName = document.querySelector("#userName").value.trim();
-      return response.map(function(pr) {
-        // console.log(rep.title);
-        if (pr.user.login.includes(value)) {
-          var li = document.createElement("li");
-          var a = document.createElement("a");
-          a.innerText = pr.title;
-          a.href = pr.url;
-          li.appendChild(a);
-          ul.appendChild(li);
-          return pr.title;
-        }
-      });
+async function getRepos(URL, value) {
+  const data = await fetch(URL);
+  const response = await data.json();
+  var array = [];
+  response.map(function(pr) {
+    array.push({
+      login: pr.user.login,
+      title: pr.title,
+      url: pr.url
     });
-  // };
-  // "value" will be the last value of the input field, and will be updated everytime the user types a new letter
-});
+  });
+  // console.log(array);
+  postArray(array);
 
+  const interval = setInterval(() => {
+    input.addEventListener("keyup", async event => {
+      const value = event.target.value;
+      console.log(value);
+      arrayF = array.filter(item => item.login.includes(value));
+      // console.log(arrayF);
+      postArray(arrayF);
+    });
+  }, 500);
+}
+
+var input = document.querySelector("#userName");
 const URL = "https://api.github.com/repos/codeyourfuture/js-exercises/pulls";
-// getPRs(URL);
+getRepos(URL);
